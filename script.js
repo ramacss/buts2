10dda708b1f57089baaee523d8e2e11d7791e69d
ramaxdr <163369549+ramaxdr@users.noreply.github.com>
2024-05-25 15:28:46 +0700

    tes

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-$(function(){
-
-  if (!$('.envelope').hasClass('open')){
-    $('.envelope').click(function(){
+$(function() {
+  if (!$('.envelope').hasClass('open')) {
+    $('.envelope').click(function() {
       $(this).removeClass('new').addClass('open');
+      $("#song")[0].play(); // Play the audio when the envelope is opened
     });
   }
-  
 });
